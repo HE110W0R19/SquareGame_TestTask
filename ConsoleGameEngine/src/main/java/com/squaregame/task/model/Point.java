d79617d348b255448479fd05f521331540669627
Aslan Gedmishov <103126971+HE110W0R19@users.noreply.github.com>
2025-09-30 01:52:48 +0300

    Create Point class with overrides equalt and hashcode for use is in hash map/set "collections"

---
+++ ConsoleGameEngine/src/main/java/com/squaregame/task/model/Point.java
@@ -0,0 +1,34 @@
+package com.squaregame.task.model;
+
+import java.util.Objects;
+
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Point point)) {
+            return false;
+        }
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
