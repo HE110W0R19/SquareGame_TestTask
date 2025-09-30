c3ac496eab56bb602cf63bf13769710a6462e587
Aslan Gedmishov <103126971+HE110W0R19@users.noreply.github.com>
2025-09-30 17:18:41 +0300

    Created and added Game class with game logic for move, place, and finish game(without a full logic)

---
+++ ConsoleGameEngine/src/main/java/com/squaregame/task/engine/Game.java
@@ -0,0 +1,67 @@
+package com.squaregame.task.engine;
+
+import com.squaregame.task.model.Color;
+import com.squaregame.task.model.Point;
+import com.squaregame.task.players.ComputerPlayer;
+import com.squaregame.task.players.Player;
+import com.squaregame.task.players.UserPlayer;
+
+public class Game {
+    private GameState gameState = GameState.NOT_STARTED;
+    private Player player1, player2;
+    private Player move;
+    private Board board;
+
+    public String startNewGame(int N, String p1, Color c1, String p2, Color c2) {
+        if (N < 2)
+            return "Incorrect board size!";
+        if (c1 == c2)
+            return "Incorrect colors, cannot be two " + c1 + "/" + c2 + "!";
+        this.board = new Board(N);
+        this.player1 = "user".equals(p1) ? new UserPlayer(c1) : new ComputerPlayer(c1, board);
+        this.player2 = "user".equals(p2) ? new UserPlayer(c1) : new ComputerPlayer(c1, board);
+        this.move = player1;
+        this.gameState = GameState.RUNNING;
+        return null;
+    }
+
+    public String placeAndProgress(int x, int y) {
+        if (!board.in(x, y))
+            return "Out of board!";
+        if (!board.isEmpty(x, y))
+            return "Cell is occupied!";
+        return "";
+    }
+
+    public String userMove(int x, int y) {
+        if (gameState == GameState.NOT_STARTED)
+            return "Game is not started!";
+        if (gameState == GameState.FINISHED)
+            return "Game is finished!";
+        if (!move.isHuman())
+            return "Not your turn!";
+        return "";
+    }
+
+    private void maybeAutoPlay() {
+        if (gameState != GameState.RUNNING)
+            return;
+        while (!move.isHuman() && gameState == GameState.RUNNING) {
+            Point m = move.nextMove();
+            if (m == null) {
+                finishGame();
+                return;
+            }
+            String err = placeAndProgress(m.getX(), m.getY());
+            if (err != null) {
+                // Если поле занято, то ищем другое
+                continue;
+            }
+        }
+    }
+
+    private void finishGame() {
+        System.out.println("Game finished!");
+        gameState = GameState.FINISHED;
+    }
+}
